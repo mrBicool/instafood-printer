9e42d6343bd8fc19cff3069b92dba469b484ca83
David <gualvezdavid@gmail.com>
2019-06-14 18:05:51 +0800

    updates

--- app/Http/Controllers/TestPrintController.php
+++ app/Http/Controllers/TestPrintController.php
@@ -7,16 +7,21 @@ use Illuminate\Http\Request;
 use Mike42\Escpos\Printer;
 use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 
+use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
+//use Mike42\Escpos\Printer;
+
 class TestPrintController extends Controller
 {
     // 
     public function test(Request $request){
-        $connector = new WindowsPrintConnector(config( 'maintenance.printer_name'));
+
+        $connector = new NetworkPrintConnector("172.16.12.198", 9100);  
+        //$connector = new WindowsPrintConnector(config( 'maintenance.printer_name'));
         $printer = new Printer($connector);
 
          
-        $printer-> text('1234567890123456789012345678901234567890123456');
-
+        $printer->text('1234567890123456789012345678901234567890123456');
+        
         // $printer->feed();
         // $printer->setJustification(Printer::JUSTIFY_CENTER);
         // $printer->qrCode('47', Printer::QR_ECLEVEL_L,8);
@@ -24,8 +29,10 @@ class TestPrintController extends Controller
         // $printer->feed(); 
 
         // $printer->text('123456789 123456789 123456789 12'."\n");   
-        $printer->feed(3);
+        $printer->feed();
         $printer->cut();
+
+        $printer->getPrintConnector()->write(PRINTER::RS);
         $printer->close();
     }
 }
